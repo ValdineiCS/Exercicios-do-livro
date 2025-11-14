a05fb028826fff0c46be75303d280b9326f3b469
ValdineiCS <109873014+ValdineiCS@users.noreply.github.com>
2025-11-14 15:57:21 -0300

    capitulo 6 finalizado

--- capitulo2/captulo6/multa.js
+++ capitulo2/captulo6/multa.js
@@ -2,25 +2,48 @@ let inData=document.getElementById('inData');
 let inValor=document.getElementById('inValor');
 let btCalculo=document.getElementById('btCalculo');
 let outData=document.getElementById('outData');
-let outValor=document.getElementById('outValor')
+let outValor=document.getElementById('outDesconto')
 
 btCalculo.addEventListener('click', calcularData);
 
 function calcularData(){
-    let data=Number(inData.value);
+    let data=inData.value;
     let valor=Number(inValor.value);
 
-    if(data==0||valor==0|| isNaN(data) || isNaN(valor)){
+    if(data==''||valor==0 || isNaN(valor)){
         alert('Insira uma data e um valor para fazermos o cálculo')
+        inData.value;
+        return
     }
 
-    var hoje =new Date();
-    var vencimento= new Date();
 
-    var partes=data.split("-");
-    vencimento.setDate(Number(partes[2]));
-    vencimento.setMonth(Number(partes[1])-4);
-    vencimento.setFullYear(Number(partes[0]));
 
-    alert(vencimento)
+    var partes=data.split("-");//Fui na var data e dividir ela em arrays a partir do '-'
+   
+    let ano=Number(partes[0]); //parte 0=Ano
+    let mes=Number(partes[1]);//parte 1=mês
+    let dia=Number(partes[2]);//parte2=dia
+
+    let vencimento= new Date(ano, mes-1, dia); //criei a variavel vencimento com ano, mes e dia
+    let limite=new Date(vencimento.getTime()); //criei a variavel limite para peger os itens da variavel vencimento
+    limite.setDate(limite.getDate()+90) //Adicionei mais 90 dias a variavel limite
+
+   let hoje=new Date()
+
+    if(hoje>limite){
+        outData.textContent=`Prazo para desconto expirado`
+        outValor.textContent=`Valor a pagar: ${valor}`
+    }
+
+    else{
+    let desconto=valor * (20/100); //Calculei o desconto em 20% do valor
+    let valorFinal= valor-desconto; //o valor final é o valor digitado menos o desconto
+
+    let formatoBr=limite.toLocaleDateString('pt-BT') //Converti a data para o formado brasileiro
+
+    outData.textContent=`Data limite para pagamento com desconto: ${formatoBr}` //Exibi na tela 
+    outValor.textContent=`Valor com Desconto: R$${valorFinal}`;//Também exibi na tela
+}
+
+    
 }
